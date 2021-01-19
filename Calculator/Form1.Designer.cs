dad9f7a1523391108b97979b331f6463c402b2bb
YANNICK BETERAMS <YANNICK.BETERAMS@student.intecbrussel.be>
2021-01-19 16:26:32 +0100

    learning WinForms

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -319,9 +319,9 @@ namespace Calculator
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
-            this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Calculator";
             this.ResumeLayout(false);
